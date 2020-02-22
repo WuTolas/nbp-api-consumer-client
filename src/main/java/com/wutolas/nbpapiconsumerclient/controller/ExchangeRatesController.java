b96eeed0ab9332b93d14e59f9b8822dde078dffa
WuTolas <36102576+WuTolas@users.noreply.github.com>
2020-02-22 20:32:39 +0100

    added exception handler for feign

--- src/main/java/com/wutolas/nbpapiconsumerclient/controller/ExchangeRatesController.java
+++ src/main/java/com/wutolas/nbpapiconsumerclient/controller/ExchangeRatesController.java
@@ -4,23 +4,30 @@ import com.wutolas.nbpapiconsumerclient.request.ExchangeRatesRequest;
 import com.wutolas.nbpapiconsumerclient.response.ExchangeRatesResponse;
 import com.wutolas.nbpapiconsumerclient.service.ExchangeRatesService;
 import com.wutolas.nbpapiconsumerclient.validator.group.ExchangeRatesRequestOrderedChecks;
+import feign.FeignException;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/exchange-rates")
 public class ExchangeRatesController {
 
     private final ExchangeRatesService exchangeRatesService;
+    private final MessageSource messageSource;
 
-    public ExchangeRatesController(ExchangeRatesService exchangeRatesService) {
+    public ExchangeRatesController(ExchangeRatesService exchangeRatesService, MessageSource messageSource) {
         this.exchangeRatesService = exchangeRatesService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/usd")
@@ -49,4 +56,18 @@ public class ExchangeRatesController {
         return "exchangeRatesPage";
     }
 
+    @ExceptionHandler({ FeignException.NotFound.class })
+    public String noDataAvailable(HttpServletRequest req, Model model) {
+        ExchangeRatesRequest exchangeRatesRequest = new ExchangeRatesRequest();
+        String date = req.getParameter("dateFrom");
+        exchangeRatesRequest.setDateFrom(LocalDate.parse(date));
+
+        model.addAttribute("exchangeRatesRequest", exchangeRatesRequest);
+        model.addAttribute(
+                "noDataAvailable",
+                messageSource.getMessage("api.nbp.status.404", null, Locale.getDefault())
+        );
+
+        return "exchangeRatesPage";
+    }
 }
